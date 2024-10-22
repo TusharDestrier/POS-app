97373e6c1771979e3eb1968dbc9f07075e16383d
Debmalya mukherjee <debmalyamukherjee02@gmail.com>
2024-10-22 17:52:42 +0530

    new mop changes

--- src/app/pages/Root/Administration/StoreMaster/components/MOP/MOP.tsx
+++ src/app/pages/Root/Administration/StoreMaster/components/MOP/MOP.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { Checkbox } from '@/components/ui/checkbox'
+import { Checkbox } from '@/components/ui/Checkbox'
 import {
   Form,
   FormControl,
