2797e2a5201b39c0f508199ff0be5b80feba4dc3
tushar dutta <tushar.dutta@destrier.in>
2024-11-11 18:15:12 +0530

    general form re modified

--- src/app/pages/Root/Administration/GeneralSetup/components/General/General.tsx
+++ src/app/pages/Root/Administration/GeneralSetup/components/General/General.tsx
@@ -1,3 +1,5 @@
+import { useFormContext } from 'react-hook-form'
+
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -7,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
@@ -15,12 +18,14 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,  
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
 
 const General = () => {
+  const { control } = useFormContext()
+
   return (
     <Card>
       <CardHeader>
@@ -30,113 +35,220 @@ const General = () => {
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-4">
-        <div className="flex flex-col items-start justify-between">
-          <Label htmlFor="pendingSettlement">Pending Settlement Days</Label>
-          <Input id="pendingSettlement" placeholder='Pending Settlement Day' className="w-full mt-3" />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="footfallEntry">Footfall Entry required in Day Settlement</Label>
-          <RadioGroup defaultValue="comfortable" className='flex items-center gap-3 '>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="default" id="r1" />
-              <Label htmlFor="r1">Yes</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="comfortable" id="r2" />
-              <Label htmlFor="r2">No</Label>
-            </div>
-          </RadioGroup>
-        </div>
-        <div className="flex flex-col items-start justify-between">
-          <Label htmlFor="maxAllowable">Maximum Allowable Discount Policy Validation</Label>
-          <Select>
-            <SelectTrigger className="w-full mt-3">
-              <SelectValue placeholder="Select a fruit" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                <SelectItem value="apple">Apple</SelectItem>
-                <SelectItem value="banana">Banana</SelectItem>
-                <SelectItem value="blueberry">Blueberry</SelectItem>
-                <SelectItem value="grapes">Grapes</SelectItem>
-                <SelectItem value="pineapple">Pineapple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="flex flex-col items-start justify-betweenn">
-          <Label htmlFor="maxBillingAmt">Maximum Biling Amount in a Single POS Biling</Label>
-          <Input id="maxBillingAmt" defaultValue="Pedro Duarte" className="w-full mt-3"/>
-        </div>
-        <div className="flex flex-col items-start justify-betweenn">
-          <Label htmlFor="panNo">PAN No. Mandetiry if Billing Amount Exceeds</Label>
-          <Input id="panNo" placeholder="Pan No" className="w-full mt-3"/>
-        </div>
-        <div className="flex flex-col items-start justify-betweenn">
-          <Label htmlFor="creditCardDetails">Credit Card Details Capture Policy</Label>
-          <Select>
-            <SelectTrigger className="w-full mt-3">
-              <SelectValue placeholder="Select credit Card Option" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                <SelectItem value="apple">Apple</SelectItem>
-                <SelectItem value="banana">Banana</SelectItem>
-                <SelectItem value="blueberry">Blueberry</SelectItem>
-                <SelectItem value="grapes">Grapes</SelectItem>
-                <SelectItem value="pineapple">Pineapple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="flex flex-col items-start justify-betweenn">
-          <Label htmlFor="creditCardDetails">Is Credit Card Authorization No. Entry Mandetory</Label>
-          <RadioGroup defaultValue="comfortable" className="flex items-center gap-3 mt-3">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="default" id="r1" className="flex items-center gap-3"/>
-              <Label htmlFor="r1">Yes</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="comfortable" id="r2" />
-              <Label htmlFor="r2">No</Label>
-            </div>
-          </RadioGroup>
-        </div>
-        <div className="flex flex-col items-start justify-between">
-          <Label htmlFor="creditCardDetails">Allow Backdate Entry</Label>
-          <RadioGroup defaultValue="comfortable" className="flex items-center gap-3 w-full mt-3">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="default" id="r1" />
-              <Label htmlFor="r1">Yes</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="comfortable" id="r2" />
-              <Label htmlFor="r2">No</Label>
-            </div>
-          </RadioGroup>
-        </div>
-        <div className="flex flex-col items-start justify-between">
-          <Label htmlFor="maxAllowable">Back Date Entry Days</Label>
-          <Input id="backDate" placeholder='Back Date Entry' className='w-full mt-3' />
-        </div>
-        <div className="flex flex-col items-start justify-between">
-          <Label htmlFor="maxAllowable">Negative Stock Checking Mode</Label>
-          <Select>
-            <SelectTrigger className="w-full mt-3">
-              <SelectValue placeholder="Select a fruit" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Choose Any One</SelectLabel>
-                <SelectItem value="apple">Stop</SelectItem>
-                <SelectItem value="banana">Ignore</SelectItem>
-                <SelectItem value="blueberry">Notify</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select> 
-        </div>
+        <FormField
+          control={control}
+          name="pendingSettlement"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Pending Settlement Days</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  id="pendingSettlement"
+                  placeholder="Pending Settlement Day"
+                  className="w-full mt-3"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="footfallEntry"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Footfall Entry required in Day Settlement</FormLabel>
+              <FormControl>
+                <RadioGroup {...field} className="flex items-center gap-3">
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="default" id="r1" />
+                    <Label htmlFor="r1">Yes</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="comfortable" id="r2" />
+                    <Label htmlFor="r2">No</Label>
+                  </div>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="maxAllowable"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Maximum Allowable Discount Policy Validation</FormLabel>
+              <FormControl>
+                <Select {...field}>
+                  <SelectTrigger className="w-full mt-3">
+                    <SelectValue placeholder="Select a fruit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Fruits</SelectLabel>
+                      <SelectItem value="apple">Apple</SelectItem>
+                      <SelectItem value="banana">Banana</SelectItem>
+                      <SelectItem value="blueberry">Blueberry</SelectItem>
+                      <SelectItem value="grapes">Grapes</SelectItem>
+                      <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="maxBillingAmt"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Maximum Billing Amount in a Single POS Billing</FormLabel>
+              <FormControl>
+                <Input {...field} id="maxBillingAmt" className="w-full mt-3" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="panNo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>PAN No. Mandatory if Billing Amount Exceeds</FormLabel>
+              <FormControl>
+                <Input {...field} id="panNo" placeholder="Pan No" className="w-full mt-3" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="creditCardDetails"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Credit Card Details Capture Policy</FormLabel>
+              <FormControl>
+                <Select {...field}>
+                  <SelectTrigger className="w-full mt-3">
+                    <SelectValue placeholder="Select credit Card Option" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Fruits</SelectLabel>
+                      <SelectItem value="apple">Apple</SelectItem>
+                      <SelectItem value="banana">Banana</SelectItem>
+                      <SelectItem value="blueberry">Blueberry</SelectItem>
+                      <SelectItem value="grapes">Grapes</SelectItem>
+                      <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="creditCardAuth"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Is Credit Card Authorization No. Entry Mandatory</FormLabel>
+              <FormControl>
+                <RadioGroup {...field} className="flex items-center gap-3 mt-3">
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="default" id="r3" />
+                    <Label htmlFor="r3">Yes</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="comfortable" id="r4" />
+                    <Label htmlFor="r4">No</Label>
+                  </div>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="backDateEntry"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Allow Backdate Entry</FormLabel>
+              <FormControl>
+                <RadioGroup {...field} className="flex items-center gap-3 w-full mt-3">
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="default" id="r5" />
+                    <Label htmlFor="r5">Yes</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="comfortable" id="r6" />
+                    <Label htmlFor="r6">No</Label>
+                  </div>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="backDateDays"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Back Date Entry Days</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  id="backDateDays"
+                  placeholder="Back Date Entry"
+                  className="w-full mt-3"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={control}
+          name="stockCheck"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Credit Card Details Capture Policy</FormLabel>
+              <FormControl>
+                <Select {...field}>
+                  <SelectTrigger className="w-full mt-3">
+                    <SelectValue placeholder="Select credit Card Option" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Fruits</SelectLabel>
+                      <SelectItem value="apple">Apple</SelectItem>
+                      <SelectItem value="banana">Banana</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </CardContent>
       <CardFooter>
         <Button>Save changes</Button>
