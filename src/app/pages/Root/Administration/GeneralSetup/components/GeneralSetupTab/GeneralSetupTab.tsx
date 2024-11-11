e514b773cb588ce2f732d1b0e664af891ea749b3
Debmalya mukherjee <debmalyamukherjee02@gmail.com>
2024-11-11 15:10:41 +0530

    Add General Tab in GeneralSetup Module

--- src/app/pages/Root/Administration/GeneralSetup/components/GeneralSetupTab/GeneralSetupTab.tsx
+++ src/app/pages/Root/Administration/GeneralSetup/components/GeneralSetupTab/GeneralSetupTab.tsx
@@ -1,3 +1,5 @@
+import General from "../General/General"
+
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -28,27 +30,7 @@ function GeneralSetupTab() {
         <TabsTrigger value="posOrder">POS Order</TabsTrigger>
       </TabsList>
       <TabsContent value="general">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-x-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
+        <General/>
       </TabsContent>
       <TabsContent value="posBIll">
         <Card>
