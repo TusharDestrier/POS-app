4885c3346676ee1cbe432272b4b39a7028899474
Debmalya mukherjee <debmalyamukherjee02@gmail.com>
2024-12-17 14:54:17 +0530

    Working on Promotion

--- src/app/pages/Root/Administration/setup/promotion/PromotionSetup/components/PromotionSetupDetailForm/PromotionSetupDetailForm.tsx
+++ src/app/pages/Root/Administration/setup/promotion/PromotionSetup/components/PromotionSetupDetailForm/PromotionSetupDetailForm.tsx
@@ -4,8 +4,12 @@ import { useFormContext } from 'react-hook-form'
 
 import { usePromotionSetupStore } from '../../store/usePromotionSetupStore'
 
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import {
   Select,
   SelectContent,
@@ -94,8 +98,8 @@ function PromotionSetupDetailForm() {
                   <SelectContent>
                     <SelectGroup>
                       <SelectLabel>Applied On </SelectLabel>
-                      <SelectItem value="wb">West Bengal</SelectItem>
-                      <SelectItem value="karnataka">Karnataka</SelectItem>
+                      <SelectItem value="eachItem">Applied on Each Item</SelectItem>
+                      <SelectItem value="billValue">Applied on Bill Value</SelectItem>
                     </SelectGroup>
                   </SelectContent>
                 </Select>
@@ -105,10 +109,37 @@ function PromotionSetupDetailForm() {
           )}
         />
       </div>
-      <div className="border border-solid border-black h-[400px] overflow-y-auto p-3 grid gap-4 grid-rows-3 items-center">
+      <div className="border border-solid border-black  h-[300px] overflow-y-auto p-3 grid gap-4 grid-rows-3 items-center">
        {/* <GrpoRequestFromTable/> */}
        <h1>Promotion Type</h1>
-       
+       <div className='border-3'>
+       <RadioGroup defaultValue="comfortable">
+      <div className="flex items-center space-x-2">
+        <Label htmlFor="r1">Free Quantity Benefit</Label>
+        <RadioGroupItem value="freequantityBenefit" id="r1" />
+      </div>
+      <div className="flex items-center space-x-2">
+        <Label htmlFor="r2">Quantity Slab Benefit</Label>
+        <RadioGroupItem value="quantityslabBenefit" id="r2" />
+      </div>
+      <div className="flex items-center space-x-2">
+        <Label htmlFor="r3">Bill Value Slab Benefit</Label>
+        <RadioGroupItem value="billvalueslabBenefit" id="r3" />
+      </div>
+    </RadioGroup>
+       </div>
+       <div className="flex items-center space-x-2">
+      <Checkbox id="terms" />
+      <label
+        htmlFor="terms"
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        Inactive
+      </label>
+    </div>
+    <div>
+        <Button>Promotion Parameters</Button>
+    </div>
       </div>
     </div>
   )
