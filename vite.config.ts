f6246a79170dc1f85c7437aa7e178aba8cd516d8
tushar dutta <tushar.dutta@destrier.in>
2025-01-28 18:19:19 +0530

    config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logLevel: 'silent', // Ignore all TypeScript errors
+    loader: 'tsx'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.ts': 'tsx',
+        '.tsx': 'tsx'
+      },
+      ignoreAnnotations: true
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
